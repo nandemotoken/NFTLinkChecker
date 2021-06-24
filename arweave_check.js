cb276980ebe54e5701248047bc195668fa534152
nandemotoken <nandemotoken@gmail.com>
2021-06-24 15:58:07 +0000

    bug

--- arweave_check.js
+++ arweave_check.js
@@ -8,15 +8,16 @@ heading = '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><title>JSO
 footing = "</body></html>"
 
 
-fs.readdir('input' , function(err , files){
+fs.readdir('input' , async function(err , files){
 	if(err) throw err;
 	//console.log(files)
-	for (f of files){
+	for (i=0; i<files.length; i++){
 	//console.log(f)
-	var url = fs.readFileSync("input/"+f , 'utf-8');
-	console.log(f)
+	const filename = files[i]
+	var url = await fs.readFileSync("input/"+filename , 'utf-8');
+	console.log(filename)
 	//console.log(text)
-	exec("wget -O - " + url , (err , stdout , stderr) =>{
+	await exec("wget -O - " + url , async (err , stdout , stderr) =>{
 	if (err){
 		      console.log(`${stderr}`)
       return
@@ -25,8 +26,8 @@ fs.readdir('input' , function(err , files){
 		json = JSON.parse(stdout)
 		//console.log(json.name)
 		data=heading+"<h1>"+json.name+'</h1><a href="'+json.external_url+'">externalURL</a><br><img src="'+json.image+'"></img>'+"<br><p>"+ json.description +"</p>" + '<video controls><source src="'+json.animation_url+'" type="video/mp4"></video>' + footing
-		fs.writeFile("output/"+f+".html" , data, (err)=>{
-			
+		await fs.writeFile("output/"+filename+".html" , data, (err)=>{
+		console.log(filename)	
 		})
 	}
 	)
